0210c62c323b1f7c72f5acf0b924c9beeba7dc04
George.P <georgipetroov@gmail.com>
2021-11-18 14:09:04 +0200

    Added CRUD methods on UserController and BookController

---
+++ src/main/java/com/bibliotek/web/BookController.java
@@ -0,0 +1,57 @@
+package com.bibliotek.web;
+
+import com.bibliotek.exception.InvalidEntityException;
+import com.bibliotek.model.Book;
+import com.bibliotek.service.BookService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/books")
+//@CrossOrigin
+@Slf4j
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public Collection<Book> getBooks() {
+        return bookService.getBooks();
+    }
+
+    @GetMapping("{id}")
+    public Book getBooks(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public Book deleteBook(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book created = bookService.createBook(book);
+        URI location = MvcUriComponentsBuilder.fromMethodName(BookController.class, "createBook", Book.class)
+                .pathSegment("{id}").buildAndExpand(created.getId()).toUri();
+        log.info("Book created: {}", book.getTitle());
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        if (book.getId() != id) {
+            throw new InvalidEntityException(
+                    String.format("Book ID=%s from path is different from Entity ID=%s", id, book.getId()));
+        }
+        Book updated = bookService.updateBook(book);
+        log.info("Book updated: {}", updated);
+        return ResponseEntity.ok(updated);
+    }
+}
--- src/main/java/com/bibliotek/web/UserController.java
+++ src/main/java/com/bibliotek/web/UserController.java
@@ -1,4 +1,57 @@
 package com.bibliotek.web;
 
+import com.bibliotek.exception.InvalidEntityException;
+import com.bibliotek.model.User;
+import com.bibliotek.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/users")
+//@CrossOrigin
+@Slf4j
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public Collection<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("{id}")
+    public User getUsers(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public User deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User created = userService.createUser(user);
+        URI location = MvcUriComponentsBuilder.fromMethodName(UserController.class, "createUser", User.class)
+                .pathSegment("{id}").buildAndExpand(created.getId()).toUri() ;
+        log.info("User created: {}", location);
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        if (user.getId() != id) {
+            throw new InvalidEntityException(
+                    String.format("User ID=%s from path is different from Entity ID=%s", id, user.getId()));
+        }
+        User updated = userService.updateUser(user);
+        log.info("User updated: {}", updated);
+        return ResponseEntity.ok(updated);
+    }
 }
