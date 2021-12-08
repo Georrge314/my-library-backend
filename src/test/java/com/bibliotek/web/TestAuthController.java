aff610427150c1ce013f9b5885af8d098801d02d
George.P <georgipetroov@gmail.com>
2021-12-08 21:53:05 +0200

    test classes created

--- src/test/java/com/bibliotek/web/TestAuthController.java
+++ src/test/java/com/bibliotek/web/TestAuthController.java
@@ -1,5 +1,40 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.bibliotek.web;
 
+import com.bibliotek.web.data.UserTestDataFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
 class TestAuthController {
 
+    private final MockMvc mockMvc;
+    private final UserTestDataFactory userTestDataFactory;
+
+    private String password = "test112233";
+
+    @Autowired
+    public TestAuthController(MockMvc mockMvc, UserTestDataFactory userTestDataFactory) {
+        this.mockMvc = mockMvc;
+        this.userTestDataFactory = userTestDataFactory;
+    }
+
+    @Test
+    public void testLoginSuccsses() {
+
+    }
+
+    @Test
+    public void testLoginFail() {
+
+    }
+
+    @Test void testRegisterSuccsses() {
+
+    }
+
+    @Test void testRegisterFail() {
+
+    }
 }
--- src/test/java/com/bibliotek/web/TestBookController.java
+++ src/test/java/com/bibliotek/web/TestBookController.java
@@ -1,5 +1,10 @@
+package com.bibliotek.web;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class TestBookController {
 
 }
