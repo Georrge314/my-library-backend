572b8f4ef411f5995ef201c5edcca5a732af411d
George.P <georgipetroov@gmail.com>
2021-12-10 17:10:07 +0200

    Added validations

--- src/main/java/com/bibliotek/domain/dto/user/UpdateUserRequest.java
+++ src/main/java/com/bibliotek/domain/dto/user/UpdateUserRequest.java
@@ -1,17 +1,21 @@
 package com.bibliotek.domain.dto.user;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Data
 public class UpdateUserRequest {
-    @NotNull
+    @Length(min = 5, max = 40)
     private String username;
 
+    @Email
     private String email;
 
+    @Length(min = 5, max = 40)
     private String fullName;
 
     private Set<String> authorities;
